601050cc7dd00776508c8972ad699924c7cb32c2
datka-datucha22 <102787951+datka-datucha22@users.noreply.github.com>
2022-04-17 14:10:58 +0400

    section3

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ window.onload = function () {
         if (window.scrollY >= 820) {
             effect.style.opacity = '1'
             effect.style.transform = 'translateX(0px)'
-            effect.style.transition = '1s ease-in-out'
+            effect.style.transition = '2s ease-in-out'
         } else {
             effect.style.opacity = '0'
             effect.style.transform = 'translateX(-50px)'
@@ -30,3 +30,21 @@ window.onload = function () {
     }
     scrollEffect()
 }
+
+// let divCircle = document.querySelector('.sec3-icon')
+// let elipse = document.querySelector('.elipse')
+// divCircle.style.transition = 'all 1.5s'
+
+// divCircle.addEventListener('mouseenter', function () {
+//     divCircle.style.borderRadius = '50%'
+//     divCircle.style.backgroundColor = '#E93656'
+//     elipse.style.opacity = '0'
+
+// })
+// divCircle.addEventListener('mouseleave', function () {
+//     divCircle.style.borderRadius = '0'
+//     divCircle.style.backgroundColor = '#131A36'
+//     elipse.style.opacity = '1'
+
+
+// })
